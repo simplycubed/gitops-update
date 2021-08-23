480a9f6ddbeb5ec81faa8a046c552ee55467ff01
hanzala1234 <42064189+hanzala1234@users.noreply.github.com>
2021-08-24 00:17:23 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -38,14 +38,10 @@ git config --global user.email "gitops-release@github.com"
 git config --global user.name "Gitops Release User"
 
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-echo "$4"
-echo "$4"  > ~/.ssh/id_gh
 chmod 600 ~/.ssh/id_gh
 
 eval `ssh-agent`
-cat ~/.ssh/id_gh
 ssh-add ~/.ssh/id_gh
-cat ~/.ssh/config
 git clone https://git@github.com:$GITHUB_ORG_AND_REPO.git  $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/simplycubed/gitops-update/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$FILE_NAME --key $KEY --value $VALUE
