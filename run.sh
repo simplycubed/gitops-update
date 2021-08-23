334bb9d3fde879f321497dd7efbd51eeecc533bf
Charles Green <cg@simplycubed.com>
2021-08-23 23:51:42 +0900

    Add error checking

--- run.sh
+++ run.sh
@@ -1,6 +1,37 @@
 #!/bin/sh -l
 set -e
 
+FILE_NAME=$1
+KEY=$2
+VALUE=$3
+GITHUB_DEPLOY_KEY=$4
+GITHUB_ORG_AND_REPO=$5
+
+if [ ! -f "${FILE_NAME}" ] && [ ! -d "${FILE_NAME}" ]; then
+  echo "FILE_NAME no value specified"
+  exit 1
+fi
+
+if [ ! -f "${KEY}" ] && [ ! -d "${KEY}" ]; then
+  echo "KEY no value specified"
+  exit 1
+fi
+
+if [ ! -f "${VALUE}" ] && [ ! -d "${VALUE}" ]; then
+  echo "VALUE no value specified"
+  exit 1
+fi
+
+if [ ! -f "${GITHUB_DEPLOY_KEY}" ] && [ ! -d "${GITHUB_DEPLOY_KEY}" ]; then
+  echo "GITHUB_DEPLOY_KEY no value specified"
+  exit 1
+fi
+
+if [ ! -f "${GITHUB_ORG_AND_REPO}" ] && [ ! -d "${GITHUB_ORG_AND_REPO}" ]; then
+  echo "GITHUB_ORG_AND_REPO no value specified"
+  exit 1
+fi
+
 mkdir -p ~/.ssh
 
 git config --global user.email "gitops-release@github.com"
@@ -12,10 +43,10 @@ chmod 600 ~/.ssh/id_rsa
 
 eval `ssh-agent`
 ssh-add ~/.ssh/id_rsa
-git clone https://git@github.com:$5.git  $RUNNER_TEMP/infra-as-code-repo
+git clone https://git@github.com:$GITHUB_ORG_AND_REPO.git  $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/simplycubed/gitops-update/master/replace-key.py
-python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$1 --key $2 --value $3
+python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$FILE_NAME --key $KEY --value $VALUE
 cd $RUNNER_TEMP/infra-as-code-repo
 git add .
-git commit -m"Release of key $2 in $1"
+git commit -m "Release of key $KEY in $FILE_NAME"
 git push
