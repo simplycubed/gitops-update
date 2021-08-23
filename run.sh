4d9a8ad934dbac950c50c0bad180dca83afd2363
hanzala <muhammadhanzala12@gmail.com>
2021-08-23 11:24:02 +0500

    Add deploy key authentication

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ set -e
 
 git config --global user.email "gitops-release@github.com"
 git config --global user.name "Gitops Release User"
-git clone https://$4@github.com/$5.git  $RUNNER_TEMP/infra-as-code-repo
-wget https://raw.githubusercontent.com/DenisPalnitsky/gitops-release/master/replace-key.py
+ssh-agent -a /tmp/ssh_agent.sock > /dev/null
+ssh-add - <<< $4
+git clone https://git@github.com:$5.git  $RUNNER_TEMP/infra-as-code-repo
+wget https://raw.githubusercontent.com/simplycubed/gitops-update/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$1 --key $2 --value $3
 cd $RUNNER_TEMP/infra-as-code-repo
 git add .
