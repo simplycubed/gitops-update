7279e108c4f17c15dfbcb76fd14ec7f4bd3c26cb
Charles Green <cg@simplycubed.com>
2021-08-24 16:55:47 +0900

    Add a forced delete between runs

--- run.sh
+++ run.sh
@@ -22,6 +22,8 @@ git config --global user.name "Gitops Update User"
 echo "$GITHUB_DEPLOY_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
+rm -rf $RUNNER_TEMP/infra-as-code-repo
+
 git clone git@github.com:$GITHUB_ORG_AND_REPO.git $RUNNER_TEMP/infra-as-code-repo
 wget https://raw.githubusercontent.com/simplycubed/gitops-update/master/replace-key.py
 python replace-key.py --file $RUNNER_TEMP/infra-as-code-repo/$FILE_NAME --key $KEY --value $VALUE
